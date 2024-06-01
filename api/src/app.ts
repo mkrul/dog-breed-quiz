a7860cb1a5391f6aec4b0ef6b0b29bf7ee109b37
mkrul <mishakrul@gmail.com>
2024-06-01 13:55:24 -0400

    change path

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.resolve('client/build')))
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve('client/build', 'index.html'));
   });
 };
