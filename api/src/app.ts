209409b934a9b456f101f0c92b810f125e07a129
mkrul <mishakrul@gmail.com>
2024-06-04 16:17:51 -0400

    sdfdwaasd

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import * as AboutRoutes from "./routes/about";
-import * as ResultsRoutes from "./routes/results";
+// import * as ResultsRoutes from "./routes/results";
 import * as UserRoutes from "./routes/user";
 import * as DogsRoutes from "./routes/dogs";
 import cors from "cors";
@@ -13,12 +13,16 @@ app.use(cors());
 
 app.use(AboutRoutes.router);
 app.use(DogsRoutes.router);
-app.use(ResultsRoutes.router);
+// app.use(ResultsRoutes.router);
 app.use(UserRoutes.router);
 
 app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
