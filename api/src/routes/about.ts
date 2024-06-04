e97709cc242fa7ea16ad8b0bf41f3b5eb31fa386
mkrul <mishakrul@gmail.com>
2024-06-04 16:11:52 -0400

    sdfdwaasd

--- api/src/routes/about.ts
+++ api/src/routes/about.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 const router = express.Router();
 
 // GET /about
 router.get("/about", (req: Request, res: Response) => {
-  res.send("About page");
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 export { router };
