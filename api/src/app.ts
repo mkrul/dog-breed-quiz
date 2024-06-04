6865927724540309054701950236daafa58ae4c1
mkrul <mishakrul@gmail.com>
2024-06-04 16:18:08 -0400

    sdfdwaasd

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
-import * as AboutRoutes from "./routes/about";
-// import * as ResultsRoutes from "./routes/results";
+// import * as AboutRoutes from "./routes/about";
+import * as ResultsRoutes from "./routes/results";
 import * as UserRoutes from "./routes/user";
 import * as DogsRoutes from "./routes/dogs";
 import cors from "cors";
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(cors());
 
-app.use(AboutRoutes.router);
+// app.use(AboutRoutes.router);
 app.use(DogsRoutes.router);
-// app.use(ResultsRoutes.router);
+app.use(ResultsRoutes.router);
 app.use(UserRoutes.router);
 
 app.use(express.static('client/build'));
