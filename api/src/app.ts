1d37ea745ca7ac9f96dc5ffeb109448092af24c7
mkrul <mishakrul@gmail.com>
2024-06-04 16:23:09 -0400

    undo

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
-// import * as AboutRoutes from "./routes/about";
+import * as AboutRoutes from "./routes/about";
 import * as ResultsRoutes from "./routes/results";
 import * as UserRoutes from "./routes/user";
 import * as DogsRoutes from "./routes/dogs";
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 
-// app.use(AboutRoutes.router);
+app.use(AboutRoutes.router);
 app.use(DogsRoutes.router);
 app.use(ResultsRoutes.router);
 app.use(UserRoutes.router);
@@ -19,10 +19,6 @@ app.use(UserRoutes.router);
 app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
-app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
