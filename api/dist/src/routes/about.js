962d15e4cf62ed46b62a3e27e59732485ba1c12d
mkrul <mishakrul@gmail.com>
2024-06-06 05:50:18 -0400

    attempting server side rendering

--- api/dist/src/routes/about.js
+++ api/dist/src/routes/about.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const router = express_1.default.Router();
 exports.router = router;
 // GET /about
 router.get("/about", (req, res) => {
-    res.send("About page");
+    return app.render("/about");
 });
--- api/src/routes/about.ts
+++ api/src/routes/about.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
-import path from "path";
+
+const app = express();
 
 const router = express.Router();
 
 // GET /about
 router.get("/about", (req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  return app.render("/about");
 });
 
 export { router };
