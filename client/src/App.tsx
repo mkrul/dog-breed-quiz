0a51ecd2518b936f63c4d0454ca63eabc3379247
Misha Krul <46890239+mkrul@users.noreply.github.com>
2024-06-06 08:22:51 -0400

    Revert "followup ssr"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "bulma/css/bulma.min.css";
+import "./App.css";
 import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
+import ResultsPage from "./components/pages/ResultsPage";
+import BreedsPage from "./components/pages/BreedsPage";
+import DnaPage from "./components/pages/DnaPage";
+import AlignmentPage from "./components/pages/AlignmentPage";
+import DogsPage from "./components/pages/DogsPage";
 
 const theme = createTheme({
   palette: {
@@ -37,19 +49,26 @@ const theme = createTheme({
   },
 });
 
-const App: React.FC = () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/test/results" element={<ResultsPage />} />
+            <Route path="/test/alignment" element={<AlignmentPage />} />
+            <Route path="/test/breeds" element={<BreedsPage />} />
+            <Route path="/test/dna" element={<DnaPage />} />
+            <Route path="/test/dogs" element={<DogsPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
       </div>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
